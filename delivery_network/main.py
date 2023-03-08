5c687801c670c19130ab90f770430958a89b3e29
cgadeau <clement.gadeau@ensae.fr>
2023-03-08 18:41:44 +0000

    .

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,9 @@
-from graph import Graph, graph_from_file
+from graph import *
 
 
 data_path = "/home/onyxia/work/ensae-prog23/input/"
 file_name = "network.01.in"
 
 g = graph_from_file(data_path + file_name)
-print(g)
+a = g.connected_components_set()
+print(g, a)
